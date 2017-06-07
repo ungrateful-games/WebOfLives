0b292113b409e5a79b919320a7b60fd401b32892
John Dunham <jfdbear1@gmail.com>
2017-06-06 21:38:40 -0400

    This doesn't really work, but I wanted to save my thought process. Bin Packing is ill suited for good dungeon generation because it makes the dungeon too regular.

--- Assets/Dungeon/DungeonPacker.cs
+++ Assets/Dungeon/DungeonPacker.cs
@@ -6,13 +6,13 @@ using UnityEngine;
 public class BinRoom
 {
     [SerializeField]
-    int X;
+    public int X;
     [SerializeField]
-    int Y;
+    public int Y;
     [SerializeField]
-    int W;
+    public int W;
     [SerializeField]
-    int H;
+    public int H;
     [SerializeField]
     bool Used;
     [SerializeField]
@@ -45,9 +45,8 @@ public class BinRoom
 
         if (NodeFound)
         {
-
-            targetNode.Right = new BinRoom(targetNode.X + w + oX, targetNode.Y         , targetNode.W - (w + oX), targetNode.H           , false);
-            targetNode.Down  = new BinRoom(targetNode.X         , targetNode.Y + h + oY, targetNode.W           , targetNode.H - (h + oY), false);
+            targetNode.Right = new BinRoom(targetNode.X + w + oX, targetNode.Y         , targetNode.W - w , targetNode.H - oY, false);
+            targetNode.Down  = new BinRoom(targetNode.X         , targetNode.Y + h + oY, targetNode.W - oX, targetNode.H - h , false);
 
             targetNode.W = w;
             targetNode.H = h;
@@ -83,6 +82,34 @@ public class BinRoom
         return retRoom;
     }
 
+    public int PopulateArray( ref BinRoom[] binRooms, int index)
+    {
+        // If the down is not null we can use it for populating the array 
+        if (this.Down != null)
+        {
+            // If the room was used we can populate it.
+            if (this.Down.Used)
+                index = this.Down.PopulateArray(ref binRooms, index);
+            else
+                this.Down = null;
+        }
+
+        // Cache the room.
+        binRooms[index++] = this;
+
+        // If the down is not null we can use it for populating the array 
+        if (this.Right != null)
+        {
+            // If the room was used we can populate it.
+            if (this.Right.Used)
+                index = this.Right.PopulateArray(ref binRooms, index);
+            else
+                this.Right = null;
+        }
+
+        return index;
+    }
+
     public override string ToString()
     {
         return "x: " + X + " y: " + Y + " W: " + W + " H:" + H;
@@ -90,19 +117,21 @@ public class BinRoom
 }
 
 [System.Serializable]
-public class DungeonPacker {
+public class DungeonPacker
+{
+    private BinRoom RootRoom;
 
     [SerializeField]
-    public BinRoom RootRoom;
+    public BinRoom[] Rooms;
 
     [SerializeField]
-    public int DungeonWidth = 50;
+    public int Width = 50;
 
     [SerializeField]
-    public int DungeonHeight = 50;
+    public int Height = 50;
 
     [SerializeField]
-    public int DungeonSeed = -1;
+    public int PlaceSeed = -1;
 
     [SerializeField]
     public int NumRooms = 0;
@@ -133,25 +162,22 @@ public class DungeonPacker {
     {
         //RootRoom.PlaceRoom();
     }
-
-
+    
     public void GenerateDungeon()
     {
         System.Random placementGen;
+
         // Init the seed of the dungeon.
-        if (this.DungeonSeed != -1)
-            placementGen = new System.Random(this.DungeonSeed);
+        if (this.PlaceSeed != -1)
+            placementGen = new System.Random(this.PlaceSeed);
         else
             placementGen = new System.Random();
-        //
-        //    Random.InitState(this.DungeonSeed);
 
-        this.RootRoom = new BinRoom(this.RoomOffsetX, this.RoomOffsetY, this.DungeonWidth - this.RoomOffsetX, this.DungeonHeight - this.RoomOffsetY, false);
+        this.RootRoom = new BinRoom(this.RoomOffsetX, this.RoomOffsetY, this.Width - (2 * this.RoomOffsetX), this.Height - (2 *this.RoomOffsetY), false);
 
-        int areaConsumed = 0;
-        int areaTotal = this.DungeonWidth * this.DungeonHeight;
-        int roomCount = 0;
-        int failureCount = 0;
+        int areaConsumed = 0, roomCount = 0, failureCount = 0;
+        int areaTotal = this.Width * this.Height;
+
         // Populate the dungeon with a bin packing algorithm.
         for (roomCount = 0, failureCount = 0; roomCount < this.NumRooms && failureCount < this.MaxFailureCount; ++roomCount)
         {
@@ -160,7 +186,8 @@ public class DungeonPacker {
             int roomArea   = roomWidth * roomHeight;
 
             // Sanity checker.
-            if ( areaConsumed + roomArea <= areaTotal && this.RootRoom.PlaceRoom(roomWidth, roomHeight, this.RoomOffsetX, this.RoomOffsetY))
+            if ( areaConsumed + roomArea <= areaTotal && 
+                this.RootRoom.PlaceRoom(roomWidth, roomHeight, this.RoomOffsetX, this.RoomOffsetY))
             {
                 areaConsumed += roomArea;
             }
@@ -170,7 +197,15 @@ public class DungeonPacker {
             }
         }
 
-        Debug.Log("Available Area: "  + areaTotal + " AreaConsumed: " + areaConsumed +  " Area Unpacked: "  + (areaTotal - areaConsumed) + " Rooms: " + roomCount + " Failures: " +  failureCount);
+        Debug.Log("Available Area: " + areaTotal + " AreaConsumed: " + areaConsumed + " Area Unpacked: " + (areaTotal - areaConsumed) + " Rooms: " + roomCount + " Failures: " + failureCount);
+
+        // Populate an array of rooms for speedier/simpler access.
+        int createdRooms = roomCount - failureCount;
+        if (createdRooms > 0)
+        {
+            this.Rooms = new BinRoom[createdRooms];
+            this.RootRoom.PopulateArray(ref this.Rooms, 0);
+        }
     }
 
 
--- Assets/Dungeon/MapGenerator.cs
+++ Assets/Dungeon/MapGenerator.cs
@@ -86,24 +86,27 @@ public class MapGenerator : MonoBehaviour {
     [Tooltip("The tile Map Object that will render the map.")]
     public GameObject TileMapPrefab;
     // TODO 
+    /*
     [Tooltip("Width of the map in tiles.")]
     public int MapWidth = 99; //: The width of the map to be generated in tiles.
 
     [Tooltip("Heght of the map in tiles.")]
     public int MapHeight = 99; //: The height of the map to be generated in tiles.
-
+    */
     public const int MaxMapSegmentWidth = 149;   //: The maximum width of a map segment in tiles.
     public const int MaxMapSegmentHeight = 149;  //: The maximum height of a map segment in tiles.
 
+    /*
     public int MaxNumRooms = 30; // The number of rooms to generate.
     public int MinRoomWidth  = 3;
     public int MinRoomHeight = 3;
     public int RoomVarianceX = 9;
     public int RoomVarianceY = 9;
+    */
 
-    public CellTypes[] Cells; // The contents of the map cells, independent of number of segments.
+    //public CellTypes[] Cells; // The contents of the map cells, independent of number of segments.
     public int[] MaskedCells;
-    public int sparseness = 2;
+    //public int sparseness = 2;
 
     public TileMap[] MapSegments;   // An Array of map segments, may not be needed ? XXX
     private int SegCountX = 0;
@@ -116,31 +119,42 @@ public class MapGenerator : MonoBehaviour {
 
     public void InitMap()
     {
-        this.Dungeon.GenerateDungeon();
-        /*
-        if (this.MapHeight % 2 == 1) this.MapHeight+=1;
-        if (this.MapWidth % 2 == 1) this.MapWidth += 1;
-
-        // TODO move somewhere else.
         this.TileMappings = new Vector2[Tileset.width * Tileset.height];
-        for(int y = this.TileResolution, index = 0; y <= Tileset.height; y+=this.TileResolution)
+        for (int y = this.TileResolution, index = 0; y <= Tileset.height; y += this.TileResolution)
         {
-            for(int x= 0; x < Tileset.width; x+=this.TileResolution, index++)
+            for (int x = 0; x < Tileset.width; x += this.TileResolution, index++)
             {
-                this.TileMappings[index] = new Vector2(x, Tileset.height -  y);
-               // Debug.Log((CellTypes)index + " "  + this.TileMappings[index].x + " " + this.TileMappings[index].y);
+                this.TileMappings[index] = new Vector2(x, Tileset.height - y);
+                // Debug.Log((CellTypes)index + " "  + this.TileMappings[index].x + " " + this.TileMappings[index].y);
+            }
+        }
+
+        this.Dungeon.GenerateDungeon();
+
+        this.MaskedCells = new int[this.Dungeon.Height * this.Dungeon.Width];
+
+        for (int i = this.Dungeon.Rooms.Length - 1; i >= 0; --i)
+        {
+            BinRoom Room = this.Dungeon.Rooms[i];
+
+            for (int y = Room.H + Room.Y -1; y >= Room.Y ; --y)
+            {
+                for (int x = Room.W + Room.X -1, index = y * this.Dungeon.Width + x; x >= Room.X && index >0 && index < this.MaskedCells.Length; --x, --index)
+                {
+                    this.MaskedCells[index] = ROOM;
+                }
             }
         }
 
         #region GenMapSegments
         // Get the number of segments on each axis and initalize the array.
-        this.SegCountX = (int)Mathf.Ceil((float)this.MapWidth / MaxMapSegmentWidth);
-        this.SegCountY = (int)Mathf.Ceil((float)this.MapHeight / MaxMapSegmentHeight);
+        this.SegCountX = (int)Mathf.Ceil((float)this.Dungeon.Width  / MaxMapSegmentWidth);
+        this.SegCountY = (int)Mathf.Ceil((float)this.Dungeon.Height / MaxMapSegmentHeight);
         this.MapSegments = new TileMap[this.SegCountX * this.SegCountY];
-        
+
         // Sizing helper variables.
-        int WidthRemaining = MapWidth;
-        int HeightRemaining = MapHeight;
+        int WidthRemaining  = this.Dungeon.Width ;
+        int HeightRemaining = this.Dungeon.Height;
         int width, height;
 
         for (int y = 0; y < this.SegCountY; y++)
@@ -160,377 +174,14 @@ public class MapGenerator : MonoBehaviour {
                 TileMap.TileSize = 1;
                 TileMap.BuildMesh();
 
-                this.MapSegments[y * this.SegCountX + x] = TileMap; 
+                this.MapSegments[y * this.SegCountX + x] = TileMap;
+                this.PaintTiles(x, y);
             }
             // Perform after Columns down
-            WidthRemaining = MapWidth;
+            WidthRemaining = this.Dungeon.Width;
             HeightRemaining -= MaxMapSegmentHeight;
         }
         #endregion
-        
-        this.GenerateDungeon();
-        */
-    }
-
-    public void GenerateDungeon()
-    {
-        this.Cells = new CellTypes[MapWidth * MapHeight]; // Default set to zero ("Bedrock").
-
-        this.MaskedCells = new int[MapHeight * MapWidth]; // Build the initial "design" for the map.
-
-        #region SealEdge
-        // Prevent the edges of the map from hosting cells the user can enter.
-        // TODO explode to two separate loops? Might be the same number of checks.
-        for (int y = 0, index = 0; y < MapHeight; y++)
-        {
-            index = y * MapWidth;
-            if (y > 0 && y < MapHeight - 1)
-            {
-                this.MaskedCells[index] = NOPASS | IN_MAZE; // "Left wall"
-                this.MaskedCells[index + MapWidth - 1] = NOPASS | IN_MAZE; // "Right Wall"
-            }
-            else
-            {
-                for (int x = 0; x < MapWidth; x++, index++)
-                {
-                    this.MaskedCells[index] = NOPASS | IN_MAZE;
-                }
-            }
-        }
-        #endregion
-
-        int numRooms = MaxNumRooms;
-        while (numRooms-- > 0)
-        {
-            PlaceRoom();
-        }
-
-        // Generate the corridors.
-        ScanCorridors();
-
-
-        for (int y = 0; y < this.SegCountY; y++)
-        {
-            for (int x = 0; x < this.SegCountX; x++)
-            {
-                this.PaintTiles( x, y );
-            }
-        }
-    }
-   
-    public void PlaceRoom()
-    {
-        // Find out where the rooms belong first.
-        #region GenRoom
-        // Compute an even numbered starting location.
-        // Room Positions are even so we can keep a border on the map.
-        // Compute an odd numbered room width (they always need to be odd for consistency.
-        // Room Widths are odd because they're inclusive widths.
-        // TODO play with weight functions.
-
-        // TODO NEED 
-        // Horizontal
-        int roomX = (int)(Random.value * (MapWidth - 4 - MinRoomWidth)) + 2;
-        if (roomX % 2 == 0) roomX++;
-
-        int roomWidth = (int)(Random.value * (RoomVarianceX)) + MinRoomWidth;
-        if (roomWidth % 2 == 0) roomWidth++;
-        int roomXMax  = Mathf.Min(roomWidth  + roomX, MapWidth - 2);
-        roomWidth = roomXMax - roomX;
-
-        //  Vertical
-        int roomY = (int)(Random.value * (MapHeight - 4 - MinRoomHeight)) + 2;
-        if (roomY % 2 == 0) roomY++;
-
-        int roomHeight = (int)(Random.value * (RoomVarianceY)) + MinRoomHeight;
-        if (roomHeight % 2 == 0) roomHeight++;
-        int roomYMax  = Mathf.Min(roomHeight + roomY, MapWidth - 2);
-        roomHeight = roomYMax - roomY;
-        #endregion
-
-        //Debug.Log(roomXMax + " " + roomYMax + " " + roomX + "  " + roomY);
-        #region RoomPlacement
-        bool validRoom = true;
-        int x = roomX, y = roomY, index = 0;
-
-
-        for (; y < roomYMax && validRoom; ++y)
-        {
-            for (x = roomX,  index = y * MapWidth + x; x < roomXMax && validRoom; ++x, ++index)
-            {
-                validRoom = validRoom && (this.MaskedCells[index] == BEDROCK);
-                this.MaskedCells[index] |= ROOM | IN_MAZE;
-            }
-        }
-        #endregion
-
-        #region RoomValidation
-        // FIXME THIS IS BROKEN?
-        // If the room was valid place the room walls.
-        // Else the room was invalid, revert the touched rooms
-        // FIXME The 
-        if (validRoom)
-        { 
-            index = 0;
-            int roomType = 0;
-
-            // FIXME Can we make this more elegant?
-            for (y = roomY -1 ; y <= roomYMax; y++) {
-                index = y * MapWidth + roomX - 1;
-
-                if (y >= roomY && y < roomYMax) {
-                    this.MaskedCells[index] = ROOM | WALL | LEFT | IN_MAZE; // "Left wall"
-                    this.MaskedCells[index + roomWidth + 1] = ROOM | WALL | RIGHT | IN_MAZE; // "Right Wall"
-                }
-                else {
-                    // Set the room type
-                    if (y == roomY) roomType = ROOM | WALL | BOTTOM | IN_MAZE;
-                    else roomType = ROOM | WALL | TOP | IN_MAZE;
-
-                    for (x = roomX; x < roomXMax + 2; x++, index++) 
-                        this.MaskedCells[index] = roomType;                    
-                }
-            }
-            
-            // TODO build the room obj
-        }
-        else
-        { 
-            for (y--, x -= 2; y >= roomY; y--, x = roomXMax - 1)
-                for (; x >= roomX; x--)
-                    this.MaskedCells[y * MapWidth + x] = BEDROCK;
-        }
-
-        #endregion
-    }
-
-    void ScanCorridors()
-    {
-        List<int> Walls = new List<int>(this.MaskedCells.Length / 2);
-
-        int yMax = this.MapHeight - 1;
-        int xMax = this.MapWidth - 1;
-        
-        for (int y = 1; y < yMax; y+=2)
-        {
-            for(int x = 1, index = y * this.MapWidth + 1 ; x < xMax; x+=2, index+=2 )
-            {
-                if (this.MaskedCells[index] == BEDROCK)
-                    PlaceCorridors(index,ref Walls);
-            }
-        }
-
-        /*
-        for(int pass = 0; pass < this.sparseness; ++pass)
-        {
-            SparsifyCorridors();
-        }
-        */
-
-    }
-
-    // TODO Probably some optimization that can be done to this code.
-    // Masks horizontally and adds the walls to the list if needed.
-    void MaskHorizontal(int index, ref List<int> Walls)
-    {
-        if ((this.MaskedCells[index - 1] & MAZE_TERM) == BEDROCK)
-        {
-            Walls.Add(index - 1);
-        }
-        this.MaskedCells[index - 1] |= LEFT | WALL | IN_MAZE;
-
-
-        if ((this.MaskedCells[index + 1] & MAZE_TERM) == BEDROCK)
-        {
-            Walls.Add(index + 1);
-        }
-        this.MaskedCells[index + 1] |= RIGHT | WALL | IN_MAZE;
-    }
-
-    // TODO Probably some optimization that can be done to this code.
-    // MasksVertically and adds the walls to the list if needed.
-    void MaskVertical(int index, ref List<int> Walls)
-    {
-        if ((this.MaskedCells[index + this.MapWidth] & MAZE_TERM) == BEDROCK)
-        {
-            Walls.Add(index + this.MapWidth);
-        }
-        this.MaskedCells[index + this.MapWidth] |= TOP | WALL | IN_MAZE;
-
-
-        // This Mask test does not work, need to improve.
-        if ((this.MaskedCells[index - this.MapWidth] & MAZE_TERM) == BEDROCK)
-        {
-            Walls.Add(index - this.MapWidth);
-        }
-        this.MaskedCells[index - this.MapWidth] |= BOTTOM | WALL | IN_MAZE;
-
-    }
-
-
-    void PlaceCorridors(int corridor, ref List<int> Walls)
-    {
-        // Since this is a spanning tree I think this might work?
-        // FIXME do the math John.
-        // TODO optimize this data structure.
-        this.MaskedCells[corridor] = CORRIDOR ;
-
-        #region BuildStartWalls
-        // For corridors and rooms we don't care if they're directional, but we can use that in the future.
-        MaskHorizontal(corridor, ref Walls);
-        MaskVertical(corridor,   ref Walls);
-        Debug.Log(Walls.Count);
-        #endregion
-
-        int junctCellIndex;
-        int dir;
-        int maxCount = 0;
-
-        // While there are "Unexplored Walls" iterate.
-        while (Walls.Count > 0)
-        {
-            //Debug.Log(Walls.Count);
-            // Get a random wall first.
-            int targetWall = (int)(Random.value * (Walls.Count - 1));
-            int wallIndex = Walls[targetWall];
-            
-
-            // Cache the wall value and extract the direction encoded.
-            dir =(this.MaskedCells[wallIndex] & DIRECTION);
-
-            switch (dir)
-            {
-                case LEFT:
-                    junctCellIndex = wallIndex - 1;
-                    break;
-                case RIGHT:
-                    junctCellIndex = wallIndex + 1;
-                    break;
-                case TOP:
-                    junctCellIndex = wallIndex + this.MapWidth;
-                    break;
-                case BOTTOM:
-                    junctCellIndex = wallIndex - this.MapWidth;
-                    break;
-                default:
-                    junctCellIndex = -1;
-                    break;
-            }
-            // Conceptually this works, but the walls get lost.
-            //Debug.Log("Direction: " + dir + " CellIndex: " + junctCellIndex + " ");
-            // If the Direction was found continue.
-            // If the direction was valid and the end cell is not already in the maze.
-            if ( junctCellIndex > 0  && (this.MaskedCells[junctCellIndex] & (CORRIDOR | NOPASS | ROOM) ) == BEDROCK)
-            {
-                // Set the wall to be a corridors.
-                this.MaskedCells[wallIndex] = CORRIDOR;
-
-                // Set the borders of the new hallway.
-                if ((dir & (LEFT | RIGHT)) != 0)
-                {
-                    this.MaskedCells[wallIndex - this.MapWidth] |= BOTTOM | WALL | IN_MAZE;
-                    this.MaskedCells[wallIndex + this.MapWidth] |=    TOP | WALL | IN_MAZE;
-                }
-                else
-                {
-                    this.MaskedCells[wallIndex + 1] |= RIGHT | WALL | IN_MAZE;
-                    this.MaskedCells[wallIndex - 1] |=  LEFT | WALL | IN_MAZE;
-                }
-
-                // Set the junction cell to be a corridor.
-                this.MaskedCells[junctCellIndex] = CORRIDOR;
-
-                // Add the walls of the junction to the frontier, iff they aren't already there.
-                MaskHorizontal(junctCellIndex, ref Walls);
-                MaskVertical(junctCellIndex, ref Walls);
-
-                // Clear out the direction from above, 2 more assignments, but I think it's quicker than a branch.
-                this.MaskedCells[wallIndex] &= CLEAR_WALL;
-            }
-            maxCount = Mathf.Max(Walls.Count, maxCount);
-            Walls.RemoveAt(targetWall);
-
-        }
-        Debug.Log("Maximum Frontier: " + maxCount);
-
-    }
-
-
-    void SparsifyCorridors()
-    {
-        int cellIndex, upIndex, downIndex, leftIndex, rightIndex, adjCorridors, safeIndex = -1;
-        for( int y = 0; y < this.MapHeight; ++y)
-        {
-            for(int x = 0; x< this.MapWidth; ++x)
-            {
-                cellIndex = y * this.MapWidth + x;
-                if (this.Cells[cellIndex] != CellTypes.Corridor) continue;
-
-                adjCorridors = 0;
-
-                if (y < (this.MapHeight -1))
-                {            
-                    upIndex = cellIndex + this.MapWidth;
-                    
-                    if(this.Cells[upIndex] == CellTypes.Room || this.Cells[upIndex] == CellTypes.Corridor )
-                    {
-                        adjCorridors++;
-                        safeIndex = upIndex;
-                    }
-                }
-                else
-                    upIndex = -1;
-
-                if (y > 0)
-                {
-                    downIndex = cellIndex - this.MapWidth;
-
-                    if (this.Cells[downIndex] == CellTypes.Room || this.Cells[downIndex] == CellTypes.Corridor)
-                    {
-                        adjCorridors++;
-                        safeIndex = downIndex;
-                    }
-                }
-                else
-                    downIndex = -1;
-
-
-                if( x > 0 )
-                {
-                    leftIndex = cellIndex - 1;
-
-                    if (this.Cells[leftIndex] == CellTypes.Room || this.Cells[leftIndex] == CellTypes.Corridor)
-                    {
-                        adjCorridors++;
-                        safeIndex = leftIndex;
-                    }
-                }
-                else
-                    leftIndex = -1;
-
-                if (x < (this.MapWidth -1))
-                {
-                    rightIndex = cellIndex + 1;
-
-                    if (this.Cells[rightIndex] == CellTypes.Room || this.Cells[rightIndex] == CellTypes.Corridor)
-                    {
-                        adjCorridors++;
-                        safeIndex = rightIndex;
-                    }
-                }
-                else
-                    rightIndex = -1;
-
-                if( adjCorridors <= 1 )
-                {
-                    if (upIndex >= 0 && safeIndex != upIndex) this.Cells[upIndex] = CellTypes.Bedrock;
-                    if (downIndex >= 0 && safeIndex != downIndex) this.Cells[downIndex] = CellTypes.Bedrock;
-                    if (rightIndex >= 0 && safeIndex != rightIndex) this.Cells[rightIndex] = CellTypes.Bedrock;
-                    if (leftIndex >= 0 && safeIndex != leftIndex) this.Cells[leftIndex] = CellTypes.Bedrock;
-                }
-            }
-        }
-
     }
 
     public void PaintTiles(int segX, int segY)
@@ -549,11 +200,10 @@ public class MapGenerator : MonoBehaviour {
         // Iterate and generate the tile map.
         for (; y < yMax; y++)
         {
-
             for (x = xStart; x < xMax; x++)
             {
-
-                Vector2 mapping = TileMappings[(int)this.MaskedCells[y * MapWidth + x] & (CELL_TYPE )];
+                //Debug.Log("x: " + x + " y: " + y + " Cell Type: " + ((int)this.MaskedCells[y * this.Dungeon.Width + x] & (CELL_TYPE)));
+                Vector2 mapping = TileMappings[(int)this.MaskedCells[y * this.Dungeon.Width + x] & (CELL_TYPE )];
                 //  Debug.Log(this.Cells[y * MapWidth + x] + " " + mapping.x + " " + mapping.y);
 
                 texture.SetPixels(x * this.TileResolution, y * this.TileResolution, this.TileResolution, this.TileResolution,
